08c93d68219a1749f8089ec80b48939f1754dda7
petya <gooseelee@kingdom.loc>
2026-01-09 13:43:49 +0300

    REFACTOR: hackRfSetting ui moved to another class

---
+++ src-java/hackrfsweep/ui/HackRFSettingsTabUI.java
@@ -0,0 +1,235 @@
+package hackrfsweep.ui;
+
+import jspectrumanalyzer.HackRFSweepSpectrumAnalyzer;
+import jspectrumanalyzer.core.HackRFSettings;
+import lombok.Getter;
+import net.miginfocom.swing.MigLayout;
+import shared.mvc.MVCController;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+public class HackRFSettingsTabUI {
+
+    private final HackRFSettings hRF;
+
+    public HackRFSettingsTabUI(HackRFSettings hackRFSettings) {
+        hRF = hackRFSettings;
+    }
+
+    private JSlider sliderGain;
+    private JSlider slider_waterfallPaletteStart;
+    private JSlider slider_waterfallPaletteSize;
+    private JSlider sliderGainVGA;
+    private JSlider sliderGainLNA;
+    private JCheckBox chckbxAntennaLNA;
+    @Getter
+    private JPanel panel;
+
+    private JPanel hackRfOptionsTab;
+
+
+    public final String getTabName() {
+        return "Опции HackRF";
+    }
+
+    public JPanel initHackRFSettingsTab() {
+        hackRfOptionsTab	= new JPanel(new MigLayout("", "[123.00px,grow,leading]", "[][][0][][][0][][][0][][][0][][][0][][0][][grow,fill]"));
+        hackRfOptionsTab.setForeground(Color.WHITE);
+        hackRfOptionsTab.setBackground(Color.BLACK);
+
+        JLabel lblGain = new JLabel("Усиление [dB]");
+        lblGain.setForeground(Color.WHITE);
+        //tab1.add(lblGain, "cell 0 0");
+
+
+        sliderGain = new JSlider(JSlider.HORIZONTAL, 0, 100, 2);
+        sliderGain.setFont(new Font("Monospaced", Font.BOLD, 16));
+        sliderGain.setBackground(Color.BLACK);
+        sliderGain.setForeground(Color.WHITE);
+        new MVCController(sliderGain, hRF.getGain());
+
+        //tab1.add(sliderGain, "flowy,cell 0 1,growx");
+
+        JLabel lbl_gainValue = new JLabel(hRF.getGain() + "dB");
+        lbl_gainValue.setForeground(Color.WHITE);
+        //tab1.add(lbl_gainValue, "cell 0 1,alignx right");
+
+        JLabel lblNewLabel_2 = new JLabel("LNA Усиление [dB]");
+        lblNewLabel_2.setForeground(Color.WHITE);
+        hackRfOptionsTab.add(lblNewLabel_2, "cell 0 3");
+
+        sliderGainLNA = new JSlider(SwingConstants.HORIZONTAL, 0, 100, 2);
+        sliderGainLNA.setForeground(Color.WHITE);
+        sliderGainLNA.setFont(new Font("Monospaced", Font.BOLD, 16));
+        sliderGainLNA.setBackground(Color.BLACK);
+
+
+        JButton leftLNAButton = new JButton("←");
+        JButton rightLNAButton = new JButton("→");
+
+        leftLNAButton.addActionListener((ActionEvent e) -> {
+            sliderGainLNA.setValue(sliderGainLNA.getValue() - 1);
+        });
+
+        rightLNAButton.addActionListener((ActionEvent e) -> {
+            sliderGainLNA.setValue(sliderGainLNA.getValue() + 1);
+        });
+
+        JPanel panelLNA = new JPanel(new BorderLayout());
+        panelLNA.setOpaque(false); // Делаем панель прозрачной
+        panelLNA.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 15)); // Отступы по 10 пикселей
+        panelLNA.add(leftLNAButton, BorderLayout.WEST);
+        panelLNA.add(sliderGainLNA, BorderLayout.CENTER);
+        panelLNA.add(rightLNAButton, BorderLayout.EAST);
+
+        hackRfOptionsTab.add(panelLNA, "cell 0 4,growx");
+
+        sliderGainLNA.setModel(new DefaultBoundedRangeModel(hRF.getGainLNA().getValue(), 0, hRF.getGainLNA().getMin(), hRF.getGainLNA().getMax()));
+        sliderGainLNA.setSnapToTicks(true);
+        sliderGainLNA.setMinorTickSpacing(hRF.getGainLNA().getStep());
+        new MVCController(sliderGainLNA, hRF.getGainLNA());
+
+
+        JLabel lblVgfaGaindb = new JLabel("VGA Усиление [dB]");
+        lblVgfaGaindb.setForeground(Color.WHITE);
+        hackRfOptionsTab.add(lblVgfaGaindb, "cell 0 6");
+
+        sliderGainVGA = new JSlider(SwingConstants.HORIZONTAL, 0, 100, 2);
+        sliderGainVGA.setForeground(Color.WHITE);
+        sliderGainVGA.setFont(new Font("Monospaced", Font.BOLD, 16));
+        sliderGainVGA.setBackground(Color.BLACK);
+
+        JButton leftVGAButton = new JButton("←");
+        JButton rightVGAButton = new JButton("→");
+
+        leftVGAButton.addActionListener((ActionEvent e) -> {
+            sliderGainVGA.setValue(sliderGainVGA.getValue() - 1);
+        });
+
+        rightVGAButton.addActionListener((ActionEvent e) -> {
+            sliderGainVGA.setValue(sliderGainVGA.getValue() + 1);
+        });
+
+        JPanel panelVGA = new JPanel(new BorderLayout());
+        panelVGA.setOpaque(false); // Делаем панель прозрачной
+        panelVGA.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 15)); // Отступы по 10 пикселей
+        panelVGA.add(leftVGAButton, BorderLayout.WEST);
+        panelVGA.add(sliderGainVGA, BorderLayout.CENTER);
+        panelVGA.add(rightVGAButton, BorderLayout.EAST);
+        hackRfOptionsTab.add(panelVGA, "cell 0 7,growx");
+        sliderGainVGA.setModel(new DefaultBoundedRangeModel(hRF.getGainVGA().getValue(), 0, hRF.getGainVGA().getMin(), hRF.getGainVGA().getMax()));
+
+        sliderGainVGA.setSnapToTicks(true);
+        sliderGainVGA.setMinorTickSpacing(hRF.getGainVGA().getStep());
+
+        new MVCController(sliderGainVGA, hRF.getGainVGA());
+
+
+        JLabel lblWaterfallPaletteStart = new JLabel("Старт палитры водопада [dB]");
+        lblWaterfallPaletteStart.setForeground(Color.WHITE);
+        hackRfOptionsTab.add(lblWaterfallPaletteStart, "cell 0 8");
+
+        slider_waterfallPaletteStart = new JSlider();
+        slider_waterfallPaletteStart.setForeground(Color.WHITE);
+        slider_waterfallPaletteStart.setBackground(Color.BLACK);
+        slider_waterfallPaletteStart.setMinimum(-100);
+        slider_waterfallPaletteStart.setMaximum(0);
+        slider_waterfallPaletteStart.setValue(-30);
+
+        JButton leftPaletteStartButton = new JButton("←");
+        JButton rightPaletteStartButton = new JButton("→");
+
+        leftPaletteStartButton.addActionListener((ActionEvent e) -> {
+            slider_waterfallPaletteStart.setValue(slider_waterfallPaletteStart.getValue() - 1);
+        });
+
+        rightPaletteStartButton.addActionListener((ActionEvent e) -> {
+            slider_waterfallPaletteStart.setValue(slider_waterfallPaletteStart.getValue() + 1);
+        });
+
+        JPanel panelPaletteStart = new JPanel(new BorderLayout());
+        panelPaletteStart.setOpaque(false); // Делаем панель прозрачной
+        panelPaletteStart.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 15)); // Отступы по 10 пикселей
+        panelPaletteStart.add(leftPaletteStartButton, BorderLayout.WEST);
+        panelPaletteStart.add(slider_waterfallPaletteStart, BorderLayout.CENTER);
+        panelPaletteStart.add(rightPaletteStartButton, BorderLayout.EAST);
+
+        hackRfOptionsTab.add(panelPaletteStart, "cell 0 9,growx");
+
+        new MVCController(slider_waterfallPaletteStart, hRF.getSpectrumPaletteStart());
+
+
+        JLabel lblWaterfallPaletteLength = new JLabel("Размер палитры водопада [dB]");
+        lblWaterfallPaletteLength.setForeground(Color.WHITE);
+        hackRfOptionsTab.add(lblWaterfallPaletteLength, "cell 0 10");
+
+
+        slider_waterfallPaletteSize = new JSlider(HackRFSweepSpectrumAnalyzer.SPECTRUM_PALETTE_SIZE_MIN, 100);
+        slider_waterfallPaletteSize.setBackground(Color.BLACK);
+        slider_waterfallPaletteSize.setForeground(Color.WHITE);
+
+
+        JButton leftPaletteSizeButton = new JButton("←");
+        JButton rightPaletteSizeButton = new JButton("→");
+
+        leftPaletteSizeButton.addActionListener((ActionEvent e) -> {
+            slider_waterfallPaletteSize.setValue(slider_waterfallPaletteSize.getValue() - 1);
+        });
+
+        rightPaletteSizeButton.addActionListener((ActionEvent e) -> {
+            slider_waterfallPaletteSize.setValue(slider_waterfallPaletteSize.getValue() + 1);
+        });
+
+        JPanel panelPaletteSize = new JPanel(new BorderLayout());
+        panelPaletteSize.setOpaque(false); // Делаем панель прозрачной
+        panelPaletteSize.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 15)); // Отступы по 10 пикселей
+        panelPaletteSize.add(leftPaletteSizeButton, BorderLayout.WEST);
+        panelPaletteSize.add(slider_waterfallPaletteSize, BorderLayout.CENTER);
+        panelPaletteSize.add(rightPaletteSizeButton, BorderLayout.EAST);
+
+        hackRfOptionsTab.add(panelPaletteSize, "cell 0 11,growx");
+        new MVCController(slider_waterfallPaletteSize, hRF.getSpectrumPaletteSize());
+
+
+
+        JLabel lblLNAEnable = new JLabel("Антенное усиление +14dB");
+        lblLNAEnable.setForeground(Color.WHITE);
+        hackRfOptionsTab.add(lblLNAEnable, "flowx,cell 0 14,growx");
+
+        chckbxAntennaLNA = new JCheckBox("");
+        chckbxAntennaLNA.setHorizontalTextPosition(SwingConstants.LEADING);
+        chckbxAntennaLNA.setBackground(Color.BLACK);
+        chckbxAntennaLNA.setForeground(Color.WHITE);
+        hackRfOptionsTab.add(chckbxAntennaLNA, "cell 0 14,alignx right");
+        new MVCController(chckbxAntennaLNA, hRF.getAntennaLNA());
+
+
+        hRF.getGain().addListener((gain) -> lbl_gainValue.setText(String.format(" %ddB  [LNA: %ddB  VGA: %ddB]",
+                gain, hRF.getGainLNA().getValue(), hRF.getGainVGA().getValue())));
+
+
+        panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        JScrollPane scrollPane = new JScrollPane(panel);
+
+        scrollPane.setPreferredSize(new Dimension(100, 150));
+        JPanel outerPanel = new JPanel(new BorderLayout());
+        outerPanel.add(scrollPane, BorderLayout.CENTER);
+        hackRfOptionsTab.add(outerPanel,"flowx,cell 0 17,growx");
+
+			/*JLabel lblAntennaPower = new JLabel("Поляризация антенны");
+			lblAntennaPower.setForeground(Color.WHITE);
+			tab1.add(lblAntennaPower, "flowx,cell 0 18,growx");
+
+			chckbxAntennaPower = new JCheckBox("");
+			chckbxAntennaPower.setHorizontalTextPosition(SwingConstants.LEADING);
+			chckbxAntennaPower.setBackground(Color.BLACK);
+			chckbxAntennaPower.setForeground(Color.WHITE);
+			tab1.add(chckbxAntennaPower, "cell 0 18,alignx right");
+			Label labelVersion = new Label("Версия: "+ Version.version);
+			tab1.add(labelVersion, "flowx,cell 0 19");*/
+        return hackRfOptionsTab;
+    }
+}
--- src-java/jspectrumanalyzer/ui/HackRFSweepSettingsUI.java
+++ src-java/jspectrumanalyzer/ui/HackRFSweepSettingsUI.java
@@ -1,5 +1,6 @@
 package jspectrumanalyzer.ui;
 
+import hackrfsweep.ui.HackRFSettingsTabUI;
 import io.reactivex.rxjava3.disposables.Disposable;
 import jspectrumanalyzer.HackRFSweepSpectrumAnalyzer;
 import jspectrumanalyzer.Version;
@@ -42,11 +43,8 @@ public class HackRFSweepSettingsUI extends JPanel implements SerifListenner {
 	private FrequencySelectorPanel frequencySelectorStart;
 	private FrequencySelectorPanel frequencySelectorEnd;
 	private JSpinner spinnerFFTBinHz;
-	private JSlider sliderGain;
 	private JSpinner spinner_numberOfSamples;
 	private JCheckBox chckbxAntennaPower;
-	private JSlider slider_waterfallPaletteStart;
-	private JSlider slider_waterfallPaletteSize;
 	private JCheckBox chckbxShowPeaks;
 	private JCheckBox chckbxRemoveSpurs;
 	private JButton btnPause;
@@ -55,9 +53,6 @@ public class HackRFSweepSettingsUI extends JPanel implements SerifListenner {
 	private JCheckBox chckbxFilterSpectrum;
 	private JSpinner spinnerPeakFallSpeed;
 	private JComboBox<FrequencyAllocationTable> comboBoxFrequencyAllocationBands;
-	private JSlider sliderGainVGA;
-	private JSlider sliderGainLNA;
-	private JCheckBox chckbxAntennaLNA;
 	private JLabel lblPeakFall;
 	private JComboBox<BigDecimal> comboBoxLineThickness;
 	private JLabel lblPersistentDisplay;
@@ -72,7 +67,6 @@ public class HackRFSweepSettingsUI extends JPanel implements SerifListenner {
 	private final Disposable subscription;
 	private Integer testValue = 0;
 	private JLabel newItem;
-	private JPanel panel;
 	private JCheckBox checkboxChartVisible;
 	private JComboBox<String> comboBoxSerifVariants;
 	private JComboBox<Integer> comboBoxCounterSignal;
@@ -85,6 +79,8 @@ public class HackRFSweepSettingsUI extends JPanel implements SerifListenner {
 	private JComboBox<Integer> comboBoxSubRange;
 	private JLabel lblSubRange;
 
+
+	private final HackRFSettingsTabUI hackRFSettingsTabUI;
 	/**
 	 * Create the panel.
 	 */
@@ -271,15 +267,14 @@ public class HackRFSweepSettingsUI extends JPanel implements SerifListenner {
 		tabbedPane.setForeground(Color.WHITE);
 		tabbedPane.setBackground(Color.BLACK);
 
-		JPanel hackRfOptionsTab	= new JPanel(new MigLayout("", "[123.00px,grow,leading]", "[][][0][][][0][][][0][][][0][][][0][][0][][grow,fill]"));
-		hackRfOptionsTab.setForeground(Color.WHITE);
-		hackRfOptionsTab.setBackground(Color.BLACK);
+		hackRFSettingsTabUI = new HackRFSettingsTabUI(hRF);
+		JPanel hackRfOptionsTab = hackRFSettingsTabUI.initHackRFSettingsTab();
 
 		JPanel GraphOptionsTab	= new JPanel(new MigLayout("", "[123.00px,grow,leading]", "[][0][][][0][][][0][][0][][][0][][0][][][0][0][][][0][][0][grow,fill]"));
 		GraphOptionsTab.setForeground(Color.WHITE);
 		GraphOptionsTab.setBackground(Color.BLACK);
 		
-		tabbedPane.addTab("Опции HackRF", hackRfOptionsTab);
+		tabbedPane.addTab(hackRFSettingsTabUI.getTabName(), hackRfOptionsTab);
 		tabbedPane.addTab("Опции графика ", GraphOptionsTab);
 		tabbedPane.setForegroundAt(1, Color.BLACK);
 		tabbedPane.setBackgroundAt(1, Color.WHITE);
@@ -288,7 +283,7 @@ public class HackRFSweepSettingsUI extends JPanel implements SerifListenner {
 		tabbedPane.setBackgroundAt(0, Color.WHITE);
 
 		//tab1
-		initHackRFSettingsTab(hackRFSettings, hackRfOptionsTab);
+
 
 		initGraphSettingsTab(GraphOptionsTab);
 
@@ -541,178 +536,7 @@ public class HackRFSweepSettingsUI extends JPanel implements SerifListenner {
 		GraphOptionsTab.add(checkBoxDebugDisplay, "cell 0 22,alignx right");
 	}
 
-	private void initHackRFSettingsTab(HackRFSettings hackRFSettings, JPanel hackRfOptionsTab) {
-		JLabel lblGain = new JLabel("Усиление [dB]");
-		lblGain.setForeground(Color.WHITE);
-		//tab1.add(lblGain, "cell 0 0");
 
-		sliderGain = new JSlider(JSlider.HORIZONTAL, 0, 100, 2);
-		sliderGain.setFont(new Font("Monospaced", Font.BOLD, 16));
-		sliderGain.setBackground(Color.BLACK);
-		sliderGain.setForeground(Color.WHITE);
-		//tab1.add(sliderGain, "flowy,cell 0 1,growx");
-
-		JLabel lbl_gainValue = new JLabel(hackRFSettings.getGain() + "dB");
-		lbl_gainValue.setForeground(Color.WHITE);
-		//tab1.add(lbl_gainValue, "cell 0 1,alignx right");
-
-		JLabel lblNewLabel_2 = new JLabel("LNA Усиление [dB]");
-		lblNewLabel_2.setForeground(Color.WHITE);
-		hackRfOptionsTab.add(lblNewLabel_2, "cell 0 3");
-
-		sliderGainLNA = new JSlider(SwingConstants.HORIZONTAL, 0, 100, 2);
-		sliderGainLNA.setForeground(Color.WHITE);
-		sliderGainLNA.setFont(new Font("Monospaced", Font.BOLD, 16));
-		sliderGainLNA.setBackground(Color.BLACK);
-
-
-		JButton leftLNAButton = new JButton("←");
-		JButton rightLNAButton = new JButton("→");
-
-		leftLNAButton.addActionListener((ActionEvent e) -> {
-			sliderGainLNA.setValue(sliderGainLNA.getValue() - 1);
-		});
-
-		rightLNAButton.addActionListener((ActionEvent e) -> {
-			sliderGainLNA.setValue(sliderGainLNA.getValue() + 1);
-		});
-
-		JPanel panelLNA = new JPanel(new BorderLayout());
-		panelLNA.setOpaque(false); // Делаем панель прозрачной
-		panelLNA.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 15)); // Отступы по 10 пикселей
-		panelLNA.add(leftLNAButton, BorderLayout.WEST);
-		panelLNA.add(sliderGainLNA, BorderLayout.CENTER);
-		panelLNA.add(rightLNAButton, BorderLayout.EAST);
-
-		hackRfOptionsTab.add(panelLNA, "cell 0 4,growx");
-
-
-		JLabel lblVgfaGaindb = new JLabel("VGA Усиление [dB]");
-		lblVgfaGaindb.setForeground(Color.WHITE);
-		hackRfOptionsTab.add(lblVgfaGaindb, "cell 0 6");
-
-		sliderGainVGA = new JSlider(SwingConstants.HORIZONTAL, 0, 100, 2);
-		sliderGainVGA.setForeground(Color.WHITE);
-		sliderGainVGA.setFont(new Font("Monospaced", Font.BOLD, 16));
-		sliderGainVGA.setBackground(Color.BLACK);
-
-		JButton leftVGAButton = new JButton("←");
-		JButton rightVGAButton = new JButton("→");
-
-		leftVGAButton.addActionListener((ActionEvent e) -> {
-			sliderGainVGA.setValue(sliderGainVGA.getValue() - 1);
-		});
-
-		rightVGAButton.addActionListener((ActionEvent e) -> {
-			sliderGainVGA.setValue(sliderGainVGA.getValue() + 1);
-		});
-
-		JPanel panelVGA = new JPanel(new BorderLayout());
-		panelVGA.setOpaque(false); // Делаем панель прозрачной
-		panelVGA.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 15)); // Отступы по 10 пикселей
-		panelVGA.add(leftVGAButton, BorderLayout.WEST);
-		panelVGA.add(sliderGainVGA, BorderLayout.CENTER);
-		panelVGA.add(rightVGAButton, BorderLayout.EAST);
-		hackRfOptionsTab.add(panelVGA, "cell 0 7,growx");
-
-		JLabel lblWaterfallPaletteStart = new JLabel("Старт палитры водопада [dB]");
-		lblWaterfallPaletteStart.setForeground(Color.WHITE);
-		hackRfOptionsTab.add(lblWaterfallPaletteStart, "cell 0 8");
-
-		slider_waterfallPaletteStart = new JSlider();
-		slider_waterfallPaletteStart.setForeground(Color.WHITE);
-		slider_waterfallPaletteStart.setBackground(Color.BLACK);
-		slider_waterfallPaletteStart.setMinimum(-100);
-		slider_waterfallPaletteStart.setMaximum(0);
-		slider_waterfallPaletteStart.setValue(-30);
-
-		JButton leftPaletteStartButton = new JButton("←");
-		JButton rightPaletteStartButton = new JButton("→");
-
-		leftPaletteStartButton.addActionListener((ActionEvent e) -> {
-			slider_waterfallPaletteStart.setValue(slider_waterfallPaletteStart.getValue() - 1);
-		});
-
-		rightPaletteStartButton.addActionListener((ActionEvent e) -> {
-			slider_waterfallPaletteStart.setValue(slider_waterfallPaletteStart.getValue() + 1);
-		});
-
-		JPanel panelPaletteStart = new JPanel(new BorderLayout());
-		panelPaletteStart.setOpaque(false); // Делаем панель прозрачной
-		panelPaletteStart.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 15)); // Отступы по 10 пикселей
-		panelPaletteStart.add(leftPaletteStartButton, BorderLayout.WEST);
-		panelPaletteStart.add(slider_waterfallPaletteStart, BorderLayout.CENTER);
-		panelPaletteStart.add(rightPaletteStartButton, BorderLayout.EAST);
-
-		hackRfOptionsTab.add(panelPaletteStart, "cell 0 9,growx");
-
-
-		JLabel lblWaterfallPaletteLength = new JLabel("Размер палитры водопада [dB]");
-		lblWaterfallPaletteLength.setForeground(Color.WHITE);
-		hackRfOptionsTab.add(lblWaterfallPaletteLength, "cell 0 10");
-
-
-		slider_waterfallPaletteSize = new JSlider(HackRFSweepSpectrumAnalyzer.SPECTRUM_PALETTE_SIZE_MIN, 100);
-		slider_waterfallPaletteSize.setBackground(Color.BLACK);
-		slider_waterfallPaletteSize.setForeground(Color.WHITE);
-
-		JButton leftPaletteSizeButton = new JButton("←");
-		JButton rightPaletteSizeButton = new JButton("→");
-
-		leftPaletteSizeButton.addActionListener((ActionEvent e) -> {
-			slider_waterfallPaletteSize.setValue(slider_waterfallPaletteSize.getValue() - 1);
-		});
-
-		rightPaletteSizeButton.addActionListener((ActionEvent e) -> {
-			slider_waterfallPaletteSize.setValue(slider_waterfallPaletteSize.getValue() + 1);
-		});
-
-		JPanel panelPaletteSize = new JPanel(new BorderLayout());
-		panelPaletteSize.setOpaque(false); // Делаем панель прозрачной
-		panelPaletteSize.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 15)); // Отступы по 10 пикселей
-		panelPaletteSize.add(leftPaletteSizeButton, BorderLayout.WEST);
-		panelPaletteSize.add(slider_waterfallPaletteSize, BorderLayout.CENTER);
-		panelPaletteSize.add(rightPaletteSizeButton, BorderLayout.EAST);
-
-		hackRfOptionsTab.add(panelPaletteSize, "cell 0 11,growx");
-
-
-		JLabel lblLNAEnable = new JLabel("Антенное усиление +14dB");
-		lblLNAEnable.setForeground(Color.WHITE);
-		hackRfOptionsTab.add(lblLNAEnable, "flowx,cell 0 14,growx");
-
-		chckbxAntennaLNA = new JCheckBox("");
-		chckbxAntennaLNA.setHorizontalTextPosition(SwingConstants.LEADING);
-		chckbxAntennaLNA.setBackground(Color.BLACK);
-		chckbxAntennaLNA.setForeground(Color.WHITE);
-		hackRfOptionsTab.add(chckbxAntennaLNA, "cell 0 14,alignx right");
-
-
-		hackRFSettings.getGain().addListener((gain) -> lbl_gainValue.setText(String.format(" %ddB  [LNA: %ddB  VGA: %ddB]",
-				gain, hackRFSettings.getGainLNA().getValue(), hackRFSettings.getGainVGA().getValue())));
-
-
-		panel = new JPanel();
-		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-		JScrollPane scrollPane = new JScrollPane(panel);
-
-		scrollPane.setPreferredSize(new Dimension(100, 150));
-		JPanel outerPanel = new JPanel(new BorderLayout());
-		outerPanel.add(scrollPane, BorderLayout.CENTER);
-		hackRfOptionsTab.add(outerPanel,"flowx,cell 0 17,growx");
-
-			/*JLabel lblAntennaPower = new JLabel("Поляризация антенны");
-			lblAntennaPower.setForeground(Color.WHITE);
-			tab1.add(lblAntennaPower, "flowx,cell 0 18,growx");
-
-			chckbxAntennaPower = new JCheckBox("");
-			chckbxAntennaPower.setHorizontalTextPosition(SwingConstants.LEADING);
-			chckbxAntennaPower.setBackground(Color.BLACK);
-			chckbxAntennaPower.setForeground(Color.WHITE);
-			tab1.add(chckbxAntennaPower, "cell 0 18,alignx right");
-			Label labelVersion = new Label("Версия: "+ Version.version);
-			tab1.add(labelVersion, "flowx,cell 0 19");*/
-	}
 
 	/**
 	 * Creates controllers for ui elements
@@ -731,13 +555,10 @@ public class HackRFSweepSettingsUI extends JPanel implements SerifListenner {
 					else
 						return spinnerModelFFTBinHz.getList().get(0);
 				});
-		new MVCController(sliderGain, hRF.getGain());
 		new MVCController(spinner_numberOfSamples, hRF.getSamples(), val -> Integer.parseInt(val.toString()), val -> val.toString());
 		//new MVCController(chckbxAntennaPower, hRF.getAntennaPowerEnable());
-		new MVCController(chckbxAntennaLNA, hRF.getAntennaLNA());
-		new MVCController(slider_waterfallPaletteStart, hRF.getSpectrumPaletteStart());
+
 		new MVCController(slider_chartForPing, hRF.linePing());
-		new MVCController(slider_waterfallPaletteSize, hRF.getSpectrumPaletteSize());
 		new MVCController(	(Consumer<FrequencyRange> valueChangedCall) ->
 								frequencyRangeSelector.addPropertyChangeListener((PropertyChangeEvent evt) -> valueChangedCall.accept(frequencyRangeSelector.getFrequencyRange()) ) ,
 							(FrequencyRange newComponentValue) -> {
@@ -759,18 +580,6 @@ public class HackRFSweepSettingsUI extends JPanel implements SerifListenner {
 		new MVCController(spinnerPeakFallSpeed, hRF.getPeakFallRate(), in -> (Integer)in, in -> in);
 
 		new MVCController(comboBoxFrequencyAllocationBands, hRF.getFrequencyAllocationTable());
-
-		sliderGainLNA.setModel(new DefaultBoundedRangeModel(hRF.getGainLNA().getValue(), 0, hRF.getGainLNA().getMin(), hRF.getGainLNA().getMax()));
-		sliderGainVGA.setModel(new DefaultBoundedRangeModel(hRF.getGainVGA().getValue(), 0, hRF.getGainVGA().getMin(), hRF.getGainVGA().getMax()));
-
-		sliderGainLNA.setSnapToTicks(true);
-		sliderGainLNA.setMinorTickSpacing(hRF.getGainLNA().getStep());
-
-		sliderGainVGA.setSnapToTicks(true);
-		sliderGainVGA.setMinorTickSpacing(hRF.getGainVGA().getStep());
-
-		new MVCController(sliderGainLNA, hRF.getGainLNA());
-		new MVCController(sliderGainVGA, hRF.getGainVGA());
 
 		new MVCController(comboBoxLineThickness, hRF.getSpectrumLineThickness());
 
@@ -849,6 +658,8 @@ public class HackRFSweepSettingsUI extends JPanel implements SerifListenner {
 			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
 			System.out.println(currentTime.format(formatter));
 			newItem = new JLabel(currentDate + " " + currentTime.format(formatter) + " " +startMHz + "-" + endMHz + ", азимут " +azimuthForSerif);
+			//TODO: what a panel????
+			JPanel panel = hackRFSettingsTabUI.getPanel();
 			panel.add(newItem);
 			panel.revalidate();  // Обновляем панель после добавления нового элемента
 			panel.repaint();
@@ -905,7 +716,10 @@ public class HackRFSweepSettingsUI extends JPanel implements SerifListenner {
 			else
 				varSerif = Objects.requireNonNull(comboBoxSerifVariants.getSelectedItem()).toString();
 			newItem = new JLabel(currentTime.format(formatter) + " " +startMHz + "-" + endMHz + ", азимут " +azimuth + ", " + "максимальный Dbm " + maxDbm + ", " +  varSerif);
+			//TODO: what a panel????
+			JPanel panel = hackRFSettingsTabUI.getPanel();
 			panel.add(newItem);
+
 			panel.revalidate();  // Обновляем панель после добавления нового элемента
 			panel.repaint();
 			// Автоматическая прокрутка вниз
